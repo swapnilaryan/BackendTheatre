03d4c91017f42495d6c958a995975611f7892e92
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2018-02-25 02:42:12 +0530

    prod

--- config.js
+++ config.js
@@ -65,12 +65,22 @@ const development = {
 };
 const production = {
     app: {
-        port: parseInt(process.env.TEST_APP_PORT) || 3000
+        port: parseInt(process.env.DEV_APP_PORT) || 9000,
+        cookieMaxAge: 10,
+        cookieSecret: 'qwerty',
+        bcryptSalt: 10,
+        theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+        omdbAPIKey: 'c3ec1184',
+        theMovieDBURL: 'http://api.themoviedb.org/3'
     },
     db: {
-        host: process.env.TEST_DB_HOST || 'localhost',
-        port: parseInt(process.env.TEST_DB_PORT) || 27017,
-        name: process.env.TEST_DB_NAME || 'test'
+        host: process.env.DEV_DB_HOST || 'localhost',
+        port: parseInt(process.env.DEV_DB_PORT) || 3306,
+        dbName: process.env.DEV_DB_NAME || 'movie_theatre',
+        user: process.env.DEV_DB_USER || 'root',
+        password: process.env.DEV_DB_PASSWORD || 'root',
+        debug: process.env.DEV_DB_DEBUG || false,
+        socketPath: process.env.DEV_DB_SOCKETPATH || ''
     }
 };
 
