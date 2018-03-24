07472737be5b68650ba90fe4094aada240c604a1
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2018-03-24 21:52:06 +0530

    Routes to movie schedule

--- routes/userClient.js
+++ routes/userClient.js
@@ -11,9 +11,10 @@ const router = express.Router();
 const upcomingMovies = require('../controllers/userClient/upcomingMovies.controller');
 const nowShowingMovies = require('../controllers/userClient/nowShowingMovies.controller');
 const movieDetails = require('../controllers/userClient/movieDetails.controller');
+const adminCurrentMovies = require('../controllers/admin/currentMovies.controller');
 
 router.get('/upcomingMovies', upcomingMovies.getUpComingMovies);
 router.get('/nowShowingMovies', nowShowingMovies.getNowShowingMovies);
 router.get('/movieDetails/:imdbID', movieDetails.getMovieDetails);
-
+router.get('/movieSchedule/:movieImdbID?', adminCurrentMovies.getMovieSchedule);
 module.exports = router;
